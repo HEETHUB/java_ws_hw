6967c4c01e5c3ca080cedbfedb09186898603c50
Heetae Cho <pank0415@gmail.com>
2023-01-22 09:21:31 +0900

    .

--- java_ws_05_2/src/com/ssafy/ws/step2/MovieManager.java
+++ java_ws_05_2/src/com/ssafy/ws/step2/MovieManager.java
@@ -1,5 +1,7 @@
 package com.ssafy.ws.step2;
 
+import java.util.Arrays;
+
 /**
  * 영화리스트를 배열로 관리하는 클래스
  * 
@@ -7,5 +9,25 @@ package com.ssafy.ws.step2;
  *
  */
 public class MovieManager {
-	//코드를 작성해주세요. 
+	final int MAX_SIZE = 100;
+	private Movie[] movieList = new Movie[MAX_SIZE];
+	private int size = 0;
+	
+	public void add(Movie movie) {
+		if (size == MAX_SIZE) {
+			System.out.println("최대 등록 수에 도달하였습니다.");
+		} else {
+			movieList[size++] = movie;
+		}
+	}
+	
+	public Movie[] getList() {
+		return Arrays.copyOfRange(movieList, 0, size);
+	}
+	
+	public Movie searchByTitle(String title) {
+		for (int i = 0; i < size ; i++) {
+			if (movieList[i].getTitle().equals(title)) return movieList[i];
+		} return null;
+	}
 }
--- java_ws_05_2/src/com/ssafy/ws/step2/MovieTest.java
+++ java_ws_05_2/src/com/ssafy/ws/step2/MovieTest.java
@@ -1,8 +1,18 @@
 package com.ssafy.ws.step2;
 
+import java.util.Arrays;
+
 public class MovieTest {
 
 	public static void main(String[] args) {
-		//코드를 작성해주세요. 
+		MovieManager MM = new MovieManager();
+		Movie m1 = new Movie(0, "헤어질 결심", "박찬욱", "로맨스", 120);
+		Movie m2 = new Movie(1, "기생충", "봉준호", "스릴러", 131);
+		
+		MM.add(m1);
+		MM.add(m2);
+		
+		System.out.println(Arrays.toString(MM.getList()));
+		System.out.println(MM.searchByTitle("헤어질 결심").getTitle());
 	}
 }
